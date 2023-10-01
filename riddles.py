e120a816aaf4e5dbcec69a4c5b7dd6714b3bb2ae
Tristena97 <tristen5220@aim.com>
2023-10-01 17:02:43 -0400

    first commit yo

---
+++ riddles.py
@@ -0,0 +1,171 @@
+
+print("Riddle me this!")
+print("See how many you can get correct in a row!")
+print("all answers must be in lowercase")
+
+
+riddle1 = "water"
+riddle1 = str(input("What can run but never walk?" ))
+if riddle1 == "water": 
+    print("correct!")
+else:
+    print("incorrect!")
+    exit()
+
+riddle2 = "queue"
+riddle2 = str(input("What word is pronounced the same when you take away four of its five letters?" ))
+if riddle2 == "queue":
+    print("Correct!")
+else:
+    print("Incorrect!")
+    exit()
+
+riddle3 = "plates and bowls" or "bowls and plates"
+riddle3 = str(input("You buy me to eat but you never eat me. What am I?:"))
+if riddle3 == "plates and bowls" or "bowls and plates":
+    print("correct!")
+else:
+    print("Incorrect!")
+    exit()
+
+riddle4 = "words"
+riddle4 = str(input("What can cut like a knife, sting like a bee, bite like a lion. Carry truth and lies but never move or speak. What are we?:"))
+if riddle4 == "words":
+    print("correct!")
+else:
+    print("Incorrect!")
+    exit()
+
+riddle5 = "a garbage bin with wheels"
+riddle5 = str(input("What has wheels and flies?"))
+if riddle5 == "a garbage bin with wheels":
+    print("correct!")
+else:
+    print("Incorrect!")
+    exit()
+
+
+riddle6 = "a hat"
+riddle6 = str(input( "I sit above you without judging you and can protect you and keep you warm. What am I?"))
+if riddle6== "a hat":
+    print("correct!")
+else:
+    print("Incorrect!")
+    exit()
+
+
+riddle7 = "time"
+riddle7 = str(input("I can heal and kill without touching you. What am I?"))
+if riddle7 == "time":
+    print("correct!")
+else:
+    print("Incorrect!")
+    exit()
+
+riddle8 = "in a dictionary"
+riddle8 = str(input("When is the only time that today and tomorrow come before yesterday?"))
+if riddle8 == "in a dictionary":
+    print("correct!")
+else:
+    print("Incorrect!")
+    exit()
+
+riddle9 = "a clock"
+riddle9 = str(input("I have a face and hands but my hands can never touch my face. What am I?"))
+if riddle9 == "a clock":
+    print("correct!")
+else:
+    print("Incorrect!")
+    exit()
+
+riddle10 = "the wind"
+riddle10 = str(input("What can bring tears to your eyes, make you move faster, make you move slower and knock you over?"))
+if riddle10 == "the wind":
+    print("correct!")
+else:
+    print("Incorrect!")
+    exit()
+
+riddle11 = "samuel"
+riddle11 = str(input("Samuel’s father has three sons: David, Andrew and..?" ))
+if riddle11 == "samuel": 
+    print("correct!")
+else:
+    print("incorrect!")
+    exit()
+
+riddle12 = "a candle"
+riddle12 = str(input("I get shorter as I get older. What am I?" ))
+if riddle12 == "a candle": 
+    print("correct!")
+else:
+    print("incorrect!")
+    exit()
+
+riddle13 = "a deck of cards"
+riddle13 = str(input("What has fifteen diamonds but isn’t rich?" ))
+if riddle13 == "a deck of cards": 
+    print("correct!")
+else:
+    print("incorrect!")
+    exit()
+
+riddle14 = "a book"
+riddle14 = str(input("What has leaves but no branches?" ))
+if riddle14 == "a book": 
+    print("correct!")
+else:
+    print("incorrect!")
+    exit()
+
+riddle15 = "a glass wont smash a hard floor"
+riddle15 = str(input("How can you drop a glass onto a hard floor without smashing it?" ))
+if riddle15 == "a glass wont smash a hard floor": 
+    print("correct!")
+else:
+    print("incorrect!")
+    exit()
+
+
+riddle16 = "on a clock"
+riddle16 = str(input("When does 11 + 3 = 2?" ))
+if riddle16 == "on a clock": 
+    print("correct!")
+else:
+    print("incorrect!")
+    exit()
+
+riddle17 = "because you stop looking when you find it"
+riddle17 = str(input("Why is it that when you have lost something, it’s always in the last place you look?" ))
+if riddle17 == "because you stop looking when you find i": 
+    print("correct!")
+else:
+    print("incorrect!")
+    exit()
+
+riddle18 = "a donut"
+riddle18 = str(input("What has no beginning, middle or end?" ))
+if riddle18 == "a donut": 
+    print("correct!")
+else:
+    print("incorrect!")
+    exit()
+
+riddle19 = "double l"
+riddle19 = str(input("What is at the head of a Llama and at the tail of a seagull?" ))
+if riddle19 == "double l": 
+    print("correct!")
+else:
+    print("incorrect!")
+    exit()
+
+riddle20 = "age"
+riddle20 = str(input("What goes up and never comes down?" ))
+if riddle20 == "age": 
+    print("correct!")
+else:
+    print("incorrect!")
+    exit()
+
+print("congratulations! You got all 20 correct!")
+
